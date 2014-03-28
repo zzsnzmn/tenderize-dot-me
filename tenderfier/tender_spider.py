00e2a32e3e13c77c5110aa9449b7286b96eb6c52
david <zzsnzmn@users.noreply.github.com>
2014-03-28 09:11:10 -0500

    updated to check md5s and prevent duplicates

--- tenderfier/tender_spider.py
+++ tenderfier/tender_spider.py
@@ -1,12 +1,21 @@
 import re
+import hashlib
 import urllib2
+import sys
+sys.path.append("../")
+from tenders import *
 
 response = urllib2.urlopen("http://www.flickr.com/search/?q=chicken%20tenders")
 html = response.read()
 
 files = re.findall('(http://[^"]*\.jpg)', html)
 
-for item in enumerate(files):
-    with open("images/{}.jpg".format(item[0]), "wb") as f:
-        image = urllib2.urlopen(item[1])
-        f.write(image.read())
+for item in files:
+    img = urllib2.urlopen(item)
+    img_file = img.read()
+    m = hashlib.md5(img_file)
+    img_hash = m.hexdigest()
+    if not CONN.get(img_hash):
+        CONN.set(img_hash, True)
+    with open("images/{}.jpg".format(img_hash), "wb") as f:
+        f.write(img_file)
