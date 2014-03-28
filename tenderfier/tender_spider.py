73c086aa5e9c8f825d4422755953cdd3de29b4da
zzsnzmn <x07a69@gmail.com>
2014-03-28 14:58:24 -0500

    did stupid things for image recognition. need to use redis properly

--- tenderfier/tender_spider.py
+++ tenderfier/tender_spider.py
@@ -4,18 +4,31 @@ import urllib2
 import sys
 sys.path.append("../")
 from tenders import *
+import settings
 
 response = urllib2.urlopen("http://www.flickr.com/search/?q=chicken%20tenders")
 html = response.read()
 
-files = re.findall('(http://[^"]*\.jpg)', html)
+pics = re.findall('(http://[^"]*\.jpg)', html)
 
-for item in files:
-    img = urllib2.urlopen(item)
+for link in pics:
+    img = urllib2.urlopen(link)
     img_file = img.read()
     m = hashlib.md5(img_file)
     img_hash = m.hexdigest()
+    filename = "{}.jpg".format(img_hash)
+
     if not CONN.get(img_hash):
+        id = CONN.llen('tenders')
+        tender = {}
+
+        tender['img'] = file_name
+        tender['source'] = link
+        tender['hash'] = img_hash
+        tender['id'] = id
+
+        CONN.set(id, tender)
         CONN.set(img_hash, True)
-    with open("images/{}.jpg".format(img_hash), "wb") as f:
-        f.write(img_file)
+        CONN.rpush('tenders', img_hash)
+        with open("".join([settings.IMG_LOCATION, filename]), "wb") as f:
+            f.write(img_file)
